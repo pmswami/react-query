7b9cce456c7db8b480e615976d661be8ea554741
unknown <usern5949@gmail.com>
2024-01-05 14:36:40 +0530

    Added useQuery hook

--- starwars/src/components/People.js
+++ starwars/src/components/People.js
@@ -1,9 +1,42 @@
-const People = () => {
-    return (  
+import {  QueryClient, QueryClientProvider, useQuery } from 'react-query';
+import Person from './Person';
+const queryClient = new QueryClient();
+
+const fetchPeople = async () => {
+    const result = await fetch('https://swapi.dev/api/people')
+    return result.json()
+}
+
+const Planets = () => {
+    const { data, status } = useQuery('Planets', fetchPeople)
+    console.log("data", data, "status", status)
+    return (
         <div>
             <h2>People</h2>
+            {/* <p>{status}</p> */}
+            {status==="error" && (
+                <div>Error in fetching the data</div>
+            )}
+            {status==="loading" && (
+                <div>Loading the data</div>
+            )}
+            {status==="success" && (
+                // <div>Got you!</div>
+                <div>
+                    {data.results.map((person)=>
+                        <Person person={person} key={person.name}/>
+                    )}
+                </div>
+            )}
         </div>
     );
 }
- 
-export default People;
+
+
+export default function Wraped(){
+return(<QueryClientProvider client={queryClient}>
+        <Planets/>
+    </QueryClientProvider>
+);
+    
+}
--- starwars/src/components/Planets.js
+++ starwars/src/components/Planets.js
@@ -1,9 +1,66 @@
+// import { useQuery } from "react-query"
+// // @tanstack/react-query
+
+// const fetchPlanets = async () => {
+//     const res = await fetch("http://swapi.dev/api/planets")
+//     console.log(res.json())
+//     return res.json()
+// }
+
+// const Planets = () => {
+//     const [data, status] = useQuery("planets", fetchPlanets)
+//     console.log(data)
+
+//     return (  
+//         <div>
+//             <h2>Planets</h2>
+//         </div>
+//     );
+// }
+ 
+// export default Planets;
+
+
+import {  QueryClient, QueryClientProvider, useQuery } from 'react-query';
+import Planet from './Planet';
+
+const queryClient = new QueryClient();
+
+const fetchPlanets = async () => {
+    const result = await fetch('https://swapi.dev/api/planets')
+    return result.json()
+}
+
 const Planets = () => {
-    return (  
+    const { data, status } = useQuery('Planets', fetchPlanets)
+    console.log("data", data, "status", status)
+    return (
         <div>
             <h2>Planets</h2>
+            {/* <p>{status}</p> */}
+            {status==="error" && (
+                <div>Error in fetching the data</div>
+            )}
+            {status==="loading" && (
+                <div>Loading the data</div>
+            )}
+            {status==="success" && (
+                // <div>Got you!</div>
+                <div>
+                    {data.results.map((planet)=>
+                        <Planet planet={planet} key={planet.name}/>
+                    )}
+                </div>
+            )}
         </div>
     );
 }
- 
-export default Planets;
+
+
+export default function Wraped(){
+return(<QueryClientProvider client={queryClient}>
+        <Planets/>
+    </QueryClientProvider>
+);
+    
+}
